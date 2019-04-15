182e44452612f52325bc08954aa2f878e9c41f21
MartimBraga <martim.gard.braga@tecnico.ulisboa.pt>
2019-04-15 10:22:30 +0100

    Update test.m
    getting refresh rate

--- matlab_pioneer/test.m
+++ matlab_pioneer/test.m
@@ -1,14 +1,14 @@
-% sp=serial_port_start
-% pause(1);
-% %% 
-% pioneer_init(sp)
-% pause(1); 
-%%
+% % sp=serial_port_start
+% % pause(3);
+% % % 
+% % pioneer_init(sp)
+% % pause(3); 
+% % 
 pioneer_set_controls(sp, 200, 0);
-t = zeros(500,1);
-odemetry = zeros(500,3);
+t = zeros(1000,1);
+odemetry = zeros(1000,3);
 pause(1);
-for i=1:500
+for i=1:1000
     tic;
 %     pause(0.1)
     odeme = pioneer_read_odometry;
