8866c3e805a4ee146040032780e3b78bf0c0ae8c
magnuok <magnusknaedal@gmail.com>
2019-04-25 18:15:37 +0100

    Interpolation points saved to file. Dimentionts is not the same in map as in real life. Have to correct 1.58m on south hallway.

--- matlab_pioneer/controller.m
+++ matlab_pioneer/controller.m
@@ -1,64 +1,60 @@
 %% Create map
 clear all;
 close all;
-clf
-h = 0.1;
+clf;
 
-image = imread('map.pgm');
+image = imread('map_corrected.jpg');
 % Crop image to relevant area
-imageCropped = image(1:1100,1:1300);
-
+imageCropped = image(1:1150,1:1100);
 image = imageCropped < 100;
-
 % create occupancy grid. Free space is 0, for occupied space is 1
 % .yaml: Resolution of the map, meters / pixel = 0.020000
 % . BinaryOccupancyGrid: resoultion of cells per meter
-
 map = robotics.BinaryOccupancyGrid(image, 50);
-
 
 % Copy and inflate the map to factor in the robot's size for obstacle 
 % avoidance. Setting higher to get trajectory in middle of halway.
-robotRadius = 0.3; % TODO. Check dimention
+robotRadius = 0.3; %
 mapInflated = copy(map);
 inflate(mapInflated, robotRadius);
-
 show(mapInflated)
+hold on;
 
+ % TUNING VARIABLES
+ radius = 0.2;
+ measurment_points = 200;
+%%
  % PRM creates a roadmap path planner object for the environment map 
  % specified in the Map property.
  prm = robotics.PRM;
  prm.Map = mapInflated;
- prm.NumNodes = 2000;
+ prm.NumNodes = 2500;
  % ConnectionDistance is an upper threshold for points that are connected 
  % in the roadmap
+ 
  prm.ConnectionDistance = 3;
- 
- % From HALLWAY to ELEVATOR:
- % startLocation = [8.8 14.6];
- % endLocation = [15 12];
- 
- % From ELEVATOR to HALLWAY:
- % startLocation = [15 12];
- % endLocation = [8.8 14.6];
- 
- % From LAB to ELEVATOR:
- % startLocation = [4 16 ; 8.8 14.6];
- % endLocation = [8.8 14.6 ; 15 12];
- % measurment_points = 30;
- 
- % WHOLE HALLWAY
-  startLocation = [8.8 14.6; 12.5 1.4; 24.4 5; 20.5 17.4];
-  endLocation = [12.5 1.4; 24.4 5; 20.5 17.4; 9 14.5];
-  measurment_points = 150;
- 
- % WHOLE PATH
- % startLocation = [4 16; 8.8 14.6; 12.5 1.4; 24.4 5; 20.5 17.4; 9 14.5];
- % endLocation = [8.8 14.6; 12.5 1.4; 24.4 5; 20.5 17.4; 9 14.5; 5 17];
 
+  
+ % Lab into hallway. NEW MAP 
+ % startLocation = [2.59 20.71; 2.59 17.15; 4.37 16.07; 7.4 15];
+ % endLocation = [2.59 17.15; 4.37 16.07; 7.4 15; 7.01 12.41];
+ 
+ % HALLWAY. NEW MAP
+ % startLocation =  [7.01 12.41; 6.91 2.53; 7.59 1.91; 18.77 2.07; 19.43 2.55; 19.21 15.41; 18.49 16.05];
+ % endLocation = [6.91 2.53; 7.59 1.91; 18.77 2.07; 19.43 2.55; 19.21 15.41; 18.49 16.05; 7.19 15.89];
+  
+ % HALLWAY into LAB. NEW MAP
+ % startLocation =  [7.19 15.89; 3.811 16.22];
+ % endLocation = [3.811 16.22; 2.59 20.71];
+ 
+   % WHOLE PATH. NEW MAP
+  startLocation = [2.59 20.71; 2.59 17.15; 4.37 16.07; 7.4 15; 7.01 12.41; 6.91 2.53; 7.59 1.91; 18.77 2.07; 19.43 2.55; 19.21 15.41; 18.49 16.05; 7.19 15.89; 3.811 16.22];
+  endLocation = [2.59 17.15; 4.37 16.07; 7.4 15; 7.01 12.41; 6.91 2.53; 7.59 1.91; 18.77 2.07; 19.43 2.55; 19.21 15.41; 18.49 16.05; 7.19 15.89; 3.811 16.22; 2.59 20.71];
+  measurment_points = 200;
+  
+  
  % Search for a solution between start and end location.
  % Continue to add nodes until a path is found.
- 
  path = [];
  
  for i = 1:size(startLocation, 1)
@@ -69,7 +65,7 @@ show(mapInflated)
     iterations = 1;
     while isempty(sub_path)
         % Can tune this to add more each round
-        prm.NumNodes = prm.NumNodes + 100;
+        prm.NumNodes = prm.NumNodes + 500;
         update(prm);
         sub_path = findpath(prm, startLocation(i,:), endLocation(i,:));
         fprintf('Iteration: %i\n', iterations);
@@ -100,6 +96,8 @@ end
 
 %% INTERPOLATION AND PLOTS
 
+path = dlmread('path_nice_corrected.txt');
+
 x = path(:,1)';
 y = path(:,2)';
 
@@ -116,7 +114,6 @@ for i = 1:length(x)
 end
 
 p = pchip(x,y);
-            
 t = linspace(0,1000,length(x));
 xq = linspace(0,1000,measurment_points);
 % returns a piecewise polynomial structure
@@ -126,7 +123,7 @@ ppy = pchip(t,y);
 x_ref = ppval(ppx, xq);
 y_ref = ppval(ppy, xq);
 
-% Plotting reference trajectory
+% Plotting reference trajectory in node-map
 figure(1);
 plot(x,y,'o',x_ref,y_ref,'-','LineWidth',2);
 
@@ -137,7 +134,7 @@ x_ref = x_ref - x_ref(1);
 y_ref = y_ref - y_ref(1);
 
 % Rotate points
-theta = atan( (y_ref(2) - y_ref(1)) / (x_ref(2) - x_ref(1) ));
+theta = atan2( (y_ref(2) - y_ref(1)) , (x_ref(2) - x_ref(1) ));
 R = [cos(-theta) -sin(-theta); sin(-theta) cos(-theta)];
 trajectory_rotated = R*[x_ref ; y_ref];
 % Rotate interploation points
@@ -151,8 +148,10 @@ y = interp_roateted(2,:);
 % Calculating theta_ref
 theta_ref = zeros(1,length(x_ref));
 for i = 1: length(x_ref)-1 
-    theta_ref(i) = atan( (y_ref(i+1) - y_ref(i)) / (x_ref(i+1) - x_ref(i) ));
+    theta_ref(i) = atan2( (y_ref(i+1) - y_ref(i)), (x_ref(i+1) - x_ref(i) ));
 end
+% Setting last element to previous angle.
+theta_ref(length(x_ref)) = theta_ref(length(x_ref)-1);
 theta_ref = theta_ref - theta_ref(1);
 
 trajectory_plot = figure(2);
@@ -167,8 +166,6 @@ set(gg,"Fontsize",14);
 gg=ylabel("y - [m]");
 set(gg,"Fontsize",14);
 
-radius = 0.3;
-
 for i = 1:length(x_ref)
     %// center
     c = [x_ref(i) y_ref(i)];
@@ -176,40 +173,23 @@ for i = 1:length(x_ref)
     pos = [c-radius 2*radius 2*radius];
     rectangle('Position',pos,'Curvature',[1 1])
     axis equal
+    
+    text(x_ref(i) + 0.1,y_ref(i) + 0.1 ,num2str(i),'Color','k')
 end
 hold on;
 
-% only printing theta for tests. No meaning for whole path.
-if (size(startLocation, 1) == 1)
-    % Plotting theta_ref
-    theta_plot = figure(3);
-    axis([0,x_ref(end),0,pi])
-    gg = plot(x_ref, theta_ref, '-','LineWidth',2);
-    title('THETA')
-    hl=legend('$\theta_{ref}$', 'AutoUpdate','off');
-    set(hl,'Interpreter','latex')
-    set(gg,"LineWidth",1.5)
-    gg=xlabel("x - [m]");
-    set(gg,"Fontsize",14);
-    gg=ylabel("rad");
-    set(gg,"Fontsize",14);
-    hold on;
-end
 
 %% POSITION TRACKING
 
 % start pos = [x, y, theta]
 pose(1,:) = [x_ref(1), y_ref(1), theta_ref(1)];
-% plot start position (x,y)
-% figure(2)
-% plot(pose(1,1),pose(1,2),'ro');
 
 % observed position
 pose_obs(1,:) = pose(1,:);
 
 % Data for plotting, if needed
 e = [];
-phi = [];
+thet = [];
 alpha = [];
 v = [];
 w = [];
@@ -218,19 +198,19 @@ w = [];
 k = 1;
 
 % In Hz
-r = robotics.Rate(50);
+r = robotics.Rate(20);
 
 sp = serial_port_start();
 %CONFIG: timer_period = 0.1. Can change to lower maybe?
 pioneer_init(sp);
-pause(2);
+pause(1);
 
 for k1 = 1:length(x_ref)
     
     % Changing reference
     pose_ref = [x_ref(k1),y_ref(k1), theta_ref(k1)];
     
-    while norm(pose_obs(k,1:2) - pose_ref(1:2)) > radius % TUNING
+    while norm(pose_obs(k,1:2) - pose_ref(1:2)) > radius
         
         % this will be performed every dadada seconds
         % data = [pose_new, e, phi, alpha, v, w]
@@ -238,14 +218,14 @@ for k1 = 1:length(x_ref)
         
         pose_obs(k+1,:) = data(1:3);
         %e(k) = data(2);
-        %phi(k) = data(3);
+        %thet(k) = data(3);
         %alpha(k) = data(4);
         %v(k) = data(5);
         %w(k) = data(6);
         
         figure(2);
         hold on;
-        plot(pose_obs(k+1,1), pose_obs(k+1,2), 'k.');
+        plot(pose_obs(k+1,1), pose_obs(k+1,2), 'm.');
         drawnow;
         hold off;
         
@@ -257,13 +237,8 @@ for k1 = 1:length(x_ref)
     end
 end
 
-figure(2)
-plot(pose_obs(:,1), pose_obs(:,2), 'k.')
-% only printing theta for tests. No meaning for whole path.
-if (size(startLocation, 1) == 1)
-    figure(3)
-    plot(pose(:,1), pose(:,3), 'k.')
-end
+% figure(2)
+% plot(pose_obs(:,1), pose_obs(:,2), 'g.')
 
 pioneer_set_controls(sp, 0, 0);
 pioneer_close(sp);
@@ -272,10 +247,10 @@ stats = statistics(r)
 function data = loop(sp, pose_ref)
     
     % TUNING
-    K1 = 1; % Artikkel: 0.41 2.94 1.42 0.5
-    K2 = 1.5;
-    K3 = 1;
-    v_max=0.5;
+    K1 = 0.5; % Artikkel: 0.41 2.94 1.42 0.5
+    K2 = 2.3;
+    K3 = 1.5;
+    v_max = 1.1;
 
     % READ ODOMETRY HERE to get pose_obs
     pose_obs = pioneer_read_odometry();
@@ -289,13 +264,7 @@ function data = loop(sp, pose_ref)
     else
         pose_obs(3) = -(4096 - pose_obs(3)) * (pi / 2048);
     end
-    
-    % Calculating errors and variables
-    % e = norm(pose_ref - pose_obs(1:2));
-    % theta = atan2(pose_ref(2) - pose_obs(2), pose_ref(1) - pose_obs(1));
-    % alpha = theta - pose_obs(3);
-    
-    % TEST
+
     e = norm(pose_ref(1:2) - pose_obs(1:2));
     theta = atan2(pose_ref(2) - pose_obs(2), pose_ref(1) - pose_obs(1)) - pose_ref(3);
     alpha = theta - pose_obs(3) + pose_ref(3);
@@ -307,17 +276,20 @@ function data = loop(sp, pose_ref)
         alpha = alpha + 2*pi;
     end
     
-%     if phi>pi
-%         phi = phi-2*pi;
-%     elseif phi < -pi
-%         phi = phi + 2*pi;
-%     end
+     if theta > pi
+         theta = theta - 2*pi;
+     elseif theta < -pi
+         theta = theta + 2*pi;
+     end
 
     % Control law
-    % Put in a wind-up parameter here and tune variables?
     v = v_max*tanh(K1*e);
-    % w = v_max*((1+K2*theta)*tanh(K1*e)/e*sin(alpha)+K3*tanh(alpha));
     w = v_max*( (1+K2*(theta/alpha)) * (tanh(K1*e)/e) * sin(alpha) + K3*tanh(alpha));
+    if isnan(w)
+        w = 0;
+    elseif isnan(v)
+        v = 0;
+    end
     
     % SET v AND w here
      pioneer_set_controls(sp, round(v*1000), round(w*(180/pi)))
