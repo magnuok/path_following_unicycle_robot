4d89954def9873c4ebbbe73a9b3cc091fc5288e9
Magnus Knædal <magnuok@stud.ntnu.no>
2019-05-28 21:03:54 +0100

    Removing unused files. Ready for merging with master

--- matlab_pioneer/SetupLidar.m
+++ matlab_pioneer/SetupLidar.m
@@ -13,8 +13,8 @@ MATLAB = 1;
 
 
 if MATLAB
-    lidar = serial('COM10','baudrate',115200);
-    %lidar = serial('/dev/tty.usbmodem14201','baudrate',115200);
+    %lidar = serial('COM10','baudrate',115200);
+    lidar = serial('/dev/tty.usbmodem14201','baudrate',115200);
     set(lidar,'Timeout',0.1);   
     set(lidar,'InputBufferSize',40000);
     set(lidar,'Terminator','CR');
